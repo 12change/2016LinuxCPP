9cefa56b89bd969da365397abbd6e141898906be
Lin Shengtao <12change@tongji.edu.cn>
2016-09-24 21:20:52 +0800

    优化了性能
    1.把2和其它奇质数分离
    2.缩小了遍历的范围，使用sqrt+1
    3.更换了“之前没有除得结果”这一判定的方法，原先每次都要复制出一个string，现在只需与初始的标兵比较

--- 4.1.cpp
+++ 4.1.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
 #include <sstream>
+#include <cmath>
 using namespace std;
 int main(int argc, char const *argv[])
 {
-	int input;
+	unsigned int input;
 	stringstream result("");
 	cin >> input;
 	int INPUT = input;
-	for (int i = 2; i <= input; ++i)
+	while(input%2==0){
+		if (input==INPUT)
+			result << '2';
+		else
+			result << '*' << '2';	
+		input>>1;	
+	}
+	unsigned int t = sqrt(input)+1;
+	for (int i = 3; i <= t; i+=2)
 	{
 		while(input%i==0){
-			input/=i;
-			if (result.str()=="")
+			if (input==INPUT)
 				result << i;
 			else
-				result << '*' << i;			
+				result << '*' << i;	
+			input/=i;		
 		}
 	}
-	cout << INPUT << '=' << result.str();	
+	if(input==INPUT)
+		result << input;
+	cout << INPUT << '=' << result.str()<<endl;	
 	return 0;
 }
