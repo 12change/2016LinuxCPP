b4aae52b174e89c3a02d6c202c3150fb9ab93c2c
Lin Shengtao <12change@tongji.edu.cn>
2016-09-24 19:48:21 +0800

    今天翻开课件，发现MOOC上的例题用到了枚举类型，觉得本题最好也用那种写法
    MOOC还是要看的，不能埋头盲干

--- 2.2.cpp
+++ 2.2.cpp
@@ -1,22 +1,28 @@
-#include <iostream>
+#include <iostream>  
+#include <iomanip>
 using namespace std;
-int main(){
-	cout << "Calendar 2016-01"<<endl;
-	cout << "--------------------------------"<<endl;
-	cout << "Su   Mo   Tu   We   Th   Fr   Sa"<<endl;
-	cout << "--------------------------------"<<endl;
-	cout << "                         ";
-	int day =0;
-	while( day < 31 ){
-		day++;
-		cout << day << "   ";
-		if(day<10)
-			cout<<' ';
-		if((day+5)%7)
-			continue;
-		else 
-			cout << endl;
-	}
-	cout << endl;
-	return 0;
+typedef enum {SUN,MON,TUE,WED,THU,FRI,SAT} WEEKDAY;
+int main(int argc, char const *argv[])
+{
+    const WEEKDAY day_1 = FRI;
+    cout << "Calendar 2016-01"<<endl;
+    cout << "--------------------------"<<endl;
+    cout << "Su  Mo  Tu  We  Th  Fr  Sa"<<endl;
+    cout << "--------------------------"<<endl;
+    cout << setw(22) << '1'; 
+    for (int i = 2; i < 32; ++i)
+    {
+        switch((WEEKDAY)((day_1+i-1)%7)){
+            case SUN : cout << setw(2) << i;break;
+            case MON : cout << setw(4) << i;break;
+            case TUE : cout << setw(4) << i;break;  
+            case WED : cout << setw(4) << i;break;  
+            case THU : cout << setw(4) << i;break;
+            case FRI : cout << setw(4) << i;break;
+            case SAT : cout << setw(4) << i <<endl;break;
+
+        }
+    }
+    return 0;
 }
+  
